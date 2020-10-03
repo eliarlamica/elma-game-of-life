984d9448fb4a5a67dc640ca91a64cfc46dc656cc
Dima Bes <dmitrij.beskrestnov@gmail.com>
2020-10-04 00:47:34 +0300

    Some code shaming and fixes

--- game_logic.py
+++ game_logic.py
@@ -5,83 +5,55 @@
 #game_of_life_board_size = input("enter the board size?")
 
 game_of_life_matrix = [
-    [0, 1, 0, 1, 1],
-    [0, 0, 1, 0, 1],
-    [0, 1, 1, 0, 0],
-    [0, 0, 0, 0, 0],
-    [0, 1, 1, 1, 0]
+    [0, 1, 0],
+    [0, 1, 0],
+    [0, 1, 0],
 ]
 
-for index, cell in enumerate(game_of_life_matrix):
-    for index2, cell2 in enumerate(cell):
-        # print(cell)
-        # print(cell2)
-        neighbours_array = []
+def get_game_world():
+    for index, cell in enumerate(game_of_life_matrix):
+        for index2, cell2 in enumerate(cell):
+            neighbours_array = []
 
-        if index - 1 >= 0 and index2 - 1 >= 0:
+            if index - 1 >= 0 and index2 - 1 >= 0:
                 neighbours_array.append(game_of_life_matrix[index - 1][index2 - 1])
 
-        if index - 1 >= 0:
-            neighbours_array.append(game_of_life_matrix[index - 1][index2])
+            if index - 1 >= 0:
+                neighbours_array.append(game_of_life_matrix[index - 1][index2])
 
-        if index - 1 >= 0 and index2 + 1 <= 2:
-            neighbours_array.append(game_of_life_matrix[index - 1][index2 + 1])
+            if index - 1 >= 0 and index2 + 1 <= len(cell)-1:
+                neighbours_array.append(game_of_life_matrix[index - 1][index2 + 1])
 
-        if index2 - 1 >= 0:
-            neighbours_array.append(game_of_life_matrix[index][index2 - 1])
+            if index2 - 1 >= 0:
+                neighbours_array.append(game_of_life_matrix[index][index2 - 1])
 
-        if index2 + 1 <= 2:
-            neighbours_array.append(game_of_life_matrix[index][index2 + 1])
+            if index2 + 1 <= len(cell)-1:
+                neighbours_array.append(game_of_life_matrix[index][index2 + 1])
 
-        if index2 - 1 >= 0 and index + 1 <= 2:
-            neighbours_array.append(game_of_life_matrix[index + 1][index2 - 1])
-            
-        if index + 1 <= 2:
-            neighbours_array.append(game_of_life_matrix[index + 1][index2])
+            if index2 - 1 >= 0 and index + 1 <= len(cell)-1:
+                neighbours_array.append(game_of_life_matrix[index + 1][index2 - 1])
 
-        if index + 1 <= 2 and index2 + 1 <= 2:
-            neighbours_array.append(game_of_life_matrix[index + 1][index2 + 1])
+            if index + 1 <= len(cell)-1:
+                neighbours_array.append(game_of_life_matrix[index + 1][index2])
 
+            if index + 1 <= len(cell)-1 and index2 + 1 <= len(cell)-1:
+                neighbours_array.append(game_of_life_matrix[index + 1][index2 + 1])
 
-        # print(index)
-        # print(index2)
-        # print(index2 - 1)
-        # print(cell[index2 - 1])
-            
-        ##print(neighbours_array)
+            count = 0
+            for liveordead in neighbours_array:
+                if liveordead == 1:
+                    count += 1
 
-        count = 0
-        for liveordead in neighbours_array:
-            if liveordead == 1:
-                count += 1
+            if game_of_life_matrix[index][index2] == 0:
+                if count == 3:
+                    game_of_life_matrix[index][index2] = 1
+                else:
+                    game_of_life_matrix[index][index2] = 0
 
-        if game_of_life_matrix[index][index2] == 0:
-            if count == 3:
-                game_of_life_matrix[index][index2] = 1
-                #print("debug1")
-            else:
-                game_of_life_matrix[index][index2] = 0
-                #print("debug2")
+            if game_of_life_matrix[index][index2] == 1:
+                if count == 2 or count == 3:
+                    game_of_life_matrix[index][index2] = 1
+                else:
+                    game_of_life_matrix[index][index2] = 0
 
-        if game_of_life_matrix[index][index2] == 1:
-            if count == 2 or count == 3:
-                game_of_life_matrix[index][index2] = 1
-                #print("debug3")
-            else: 
-                game_of_life_matrix[index][index2] = 0
-                #print("debug4")
-        #print("sdfasdgvaev") 
-
-print(game_of_life_matrix)
-            
-            
-
-
-
-    #if cell == "0":
-        #live_neighbour_cells = 0
-        #if game_of_life_matrix[index - 1][index2 - 1] and game_of_life_matrix[index - 1][index], game_of_life_matrix[index - 1][index2 + 1] 
-        #print(index, index2)
-        #for game_of_life_matrix.cell[]
-
-
+    return game_of_life_matrix
--- visualization.py
+++ visualization.py
@@ -5,18 +5,25 @@ from bokeh.plotting import figure, curdoc
 from bokeh.models import Button
 from bokeh.layouts import column
 
+import game_logic
+
 _rectangles_data_source = []
+_rectangles_data_dead_source = []
 _size_of_cell = 20
 _callback_id = 0
 
 def get_game_world():
-    return [[random.randint(0,1),random.randint(0,1),random.randint(0,1)],[random.randint(0,1),random.randint(0,1),random.randint(0,1)],[random.randint(0,1),random.randint(0,1),random.randint(0,1)],[random.randint(0,1),random.randint(0,1),random.randint(0,1)],[random.randint(0,1),random.randint(0,1),random.randint(0,1)]]
+    return game_logic.get_game_world()
+    # return [[random.randint(0,1),random.randint(0,1),random.randint(0,1)],[random.randint(0,1),random.randint(0,1),random.randint(0,1)],[random.randint(0,1),random.randint(0,1),random.randint(0,1)],[random.randint(0,1),random.randint(0,1),random.randint(0,1)],[random.randint(0,1),random.randint(0,1),random.randint(0,1)]]
 
 def draw():
     live_cells_x = []
     live_cells_y = []
+    dead_cells_x = []
+    dead_cells_y = []
 
     game_world = get_game_world()
+    print(game_world)
 
     global _size_of_cell
     for x, row in enumerate(game_world):
@@ -24,10 +31,16 @@ def draw():
             if cell == 1:
                 live_cells_x.append(x*_size_of_cell)
                 live_cells_y.append(y*_size_of_cell)
+            if cell == 0:
+                dead_cells_x.append(x*_size_of_cell)
+                dead_cells_y.append(y*_size_of_cell)
 
     global _rectangles_data_source
     _rectangles_data_source.data['y'] = live_cells_x
     _rectangles_data_source.data['x'] = live_cells_y
+    global _rectangles_data_dead_source
+    _rectangles_data_dead_source.data['y'] = dead_cells_x
+    _rectangles_data_dead_source.data['x'] = dead_cells_y
 
 def start_auto_turn():
     global _callback_id
@@ -43,6 +56,10 @@ def init_canvas(canvas_width = 400, canvas_height = 400):
     rectangles = p.rect([], [], width=_size_of_cell, height=_size_of_cell, color="red", alpha=0.5)
     global _rectangles_data_source
     _rectangles_data_source = rectangles.data_source
+
+    rectangles_dead = p.rect([], [], width=_size_of_cell, height=_size_of_cell, color="black", alpha=0.2)
+    global _rectangles_data_dead_source
+    _rectangles_data_dead_source = rectangles_dead.data_source
 
     next_turn_button = Button(label="Next Turn")
     next_turn_button.on_click(draw)
