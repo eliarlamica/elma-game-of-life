42983336a6555d71ee3cd4107f2f64bf2bddf1cc
Dima Bes <dmitrij.beskrestnov@gmail.com>
2020-10-10 00:11:04 +0300

    Add randomization and glider

--- game_logic.py
+++ game_logic.py
@@ -16,15 +16,39 @@ game_of_life_matrix = []
 
 def init_world(world_size_x, world_size_y):
     global game_of_life_matrix
-    # print(world_size_x, "вот оно!")
-    # print(world_size_y, "вот оно!")
-    
-    #percent = 90
-    #nums = percent * [1] + (100 - percent) * [0]
-    game_of_life_matrix = [[random.randint(0, 1) for x in range(world_size_x)] for y in range(world_size_y)]
-    
+    game_of_life_matrix = [[0 for x in range(world_size_x)] for y in range(world_size_y)]
 
-#game_of_life_matrix_new_cell_state = copy.deepcopy(game_of_life_matrix)
+def get_world():
+    global game_of_life_matrix
+    return game_of_life_matrix;
+
+def get_world_size():
+    global game_of_life_matrix
+    world_size_x = len(game_of_life_matrix)
+    if game_of_life_matrix:
+        world_size_y = len(game_of_life_matrix[0])
+    return (world_size_x, world_size_y)
+
+def place_glider():
+    global game_of_life_matrix
+    game_of_life_matrix[1][3] = 1;
+    game_of_life_matrix[2][4] = 1;
+    game_of_life_matrix[3][2] = 1;
+    game_of_life_matrix[3][3] = 1;
+    game_of_life_matrix[3][4] = 1;
+
+def randomize_alive_cells(percent_of_cells = 50):
+    global game_of_life_matrix
+    world_size_x, world_size_y = get_world_size()
+    world_cells_count = world_size_x * world_size_y
+    alive_cells_required = int((percent_of_cells/100)*world_cells_count);
+    for i in range(alive_cells_required):
+        cell_x = random.randint(0, world_size_x-1)
+        cell_y = random.randint(0, world_size_y-1)
+        while game_of_life_matrix[cell_x][cell_y] == 1:
+            cell_x = random.randint(0, world_size_x-1)
+            cell_y = random.randint(0, world_size_y-1)
+        game_of_life_matrix[cell_x][cell_y] = 1;
 
 def get_next_turn_world():
     global game_of_life_matrix
@@ -74,7 +98,7 @@ def get_next_turn_world():
                 else:
                     temp_matrix[index][index2] = 0
 
-    game_of_life_matrix = copy.deepcopy(temp_matrix)           
+    game_of_life_matrix = copy.deepcopy(temp_matrix)
     return game_of_life_matrix
 
 # init_world(world_size_x, world_size_y)
--- run.py
+++ run.py
@@ -3,16 +3,8 @@ import visualization
 
 print("Starting Game Of Life..")
 
-world_size_x = 100
-world_size_y = 50
+world_size_x = int(input("Please provide X size of world\n"))
+world_size_y = int(input("Please provide Y size of world\n"))
 game_logic.init_world(world_size_x, world_size_y)
-#game_logic.randomize_alive_cells()
+game_logic.randomize_alive_cells()
 visualization.init_canvas()
-
-print("Game Of Life work finished. Thank You for Your time!")
-
-# TODO:
-# 1) Next Turn Fix.
-# 2) Separate random matrix generation from init_world.
-# 3) ReadMe.
-# 4) Clean Code & Code Review.
