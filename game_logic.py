90949c8965bf0c33f316f1c3688454ad8b2302f1
Леля Клер <sedrickedel@gmail.com>
2020-10-15 20:37:22 +0300

    game logic is ready for review

--- game_logic.py
+++ game_logic.py
@@ -1,18 +1,6 @@
-# Any live cell with two or three live neighbours survives.
-# Any dead cell with three live neighbours becomes a live cell.
-# All other live cells die in the next generation. Similarly, all other dead cells stay dead.
-
-#game_of_life_board_size = input("enter the board size?")
 import copy
 import random
 game_of_life_matrix = []
-
-#world_size_x = int(input("enter x"))
-#world_size_y = int(input("enter y"))
-
-#def get_next_turn_world():
-    #global game_of_life_matrix
-    #return game_of_life_matrix
 
 def init_world(world_size_x, world_size_y):
     global game_of_life_matrix
@@ -20,7 +8,7 @@ def init_world(world_size_x, world_size_y):
 
 def get_world():
     global game_of_life_matrix
-    return game_of_life_matrix;
+    return game_of_life_matrix
 
 def get_world_size():
     global game_of_life_matrix
@@ -31,7 +19,7 @@ def get_world_size():
 
 def place_glider():
     global game_of_life_matrix
-    game_of_life_matrix[1][3] = 1;
+    game_of_life_matrix[1][3] = 1; 
     game_of_life_matrix[2][4] = 1;
     game_of_life_matrix[3][2] = 1;
     game_of_life_matrix[3][3] = 1;
@@ -53,55 +41,50 @@ def randomize_alive_cells(percent_of_cells = 50):
 def get_next_turn_world():
     global game_of_life_matrix
     temp_matrix = copy.deepcopy(game_of_life_matrix)
-    for index, cell in enumerate(game_of_life_matrix):
-        for index2, cell2 in enumerate(cell):
+    for row_number, row_in_matrix in enumerate(game_of_life_matrix):
+        for cell_number, cell_state in enumerate(row_in_matrix):
             neighbours_array = []
 
-            if index - 1 >= 0 and index2 - 1 >= 0:
-                neighbours_array.append(game_of_life_matrix[index - 1][index2 - 1])
+            if row_number - 1 >= 0 and cell_number - 1 >= 0:
+                neighbours_array.append(game_of_life_matrix[row_number - 1][cell_number - 1])
 
-            if index - 1 >= 0:
-                neighbours_array.append(game_of_life_matrix[index - 1][index2])
+            if row_number - 1 >= 0:
+                neighbours_array.append(game_of_life_matrix[row_number - 1][cell_number])
 
-            if index - 1 >= 0 and index2 + 1 <= len(cell)-1:
-                neighbours_array.append(game_of_life_matrix[index - 1][index2 + 1])
+            if row_number - 1 >= 0 and cell_number + 1 <= len(row_in_matrix)-1:
+                neighbours_array.append(game_of_life_matrix[row_number - 1][cell_number + 1])
 
-            if index2 - 1 >= 0:
-                neighbours_array.append(game_of_life_matrix[index][index2 - 1])
+            if cell_number - 1 >= 0:
+                neighbours_array.append(game_of_life_matrix[row_number][cell_number - 1])
 
-            if index2 + 1 <= len(cell)-1:
-                neighbours_array.append(game_of_life_matrix[index][index2 + 1])
+            if cell_number + 1 <= len(row_in_matrix)-1:
+                neighbours_array.append(game_of_life_matrix[row_number][cell_number + 1])
 
-            if index2 - 1 >= 0 and index + 1 <= len(game_of_life_matrix)-1:
-                neighbours_array.append(game_of_life_matrix[index + 1][index2 - 1])
+            if cell_number - 1 >= 0 and row_number + 1 <= len(game_of_life_matrix)-1:
+                neighbours_array.append(game_of_life_matrix[row_number + 1][cell_number - 1])
 
-            if index + 1 <= len(game_of_life_matrix)-1:
-                neighbours_array.append(game_of_life_matrix[index + 1][index2])
+            if row_number + 1 <= len(game_of_life_matrix)-1:
+                neighbours_array.append(game_of_life_matrix[row_number + 1][cell_number])
 
-            if index + 1 <= len(game_of_life_matrix)-1 and index2 + 1 <= len(cell)-1:
-                neighbours_array.append(game_of_life_matrix[index + 1][index2 + 1])
+            if row_number + 1 <= len(game_of_life_matrix)-1 and cell_number + 1 <= len(row_in_matrix)-1:
+                neighbours_array.append(game_of_life_matrix[row_number + 1][cell_number + 1])
 
-            count = 0
+            alive_cells_count = 0
             for liveordead in neighbours_array:
                 if liveordead == 1:
-                    count += 1
+                    alive_cells_count += 1
 
-            if game_of_life_matrix[index][index2] == 0:
-                if count == 3:
-                    temp_matrix[index][index2] = 1
+            if cell_state == 0:
+                if alive_cells_count == 3:
+                    temp_matrix[row_number][cell_number] = 1
                 else:
-                    temp_matrix[index][index2] = 0
+                    temp_matrix[row_number][cell_number] = 0
 
-            if game_of_life_matrix[index][index2] == 1:
-                if count == 2 or count == 3:
-                    temp_matrix[index][index2] = 1
+            if cell_state == 1:
+                if alive_cells_count == 2 or alive_cells_count == 3:
+                    temp_matrix[row_number][cell_number] = 1
                 else:
-                    temp_matrix[index][index2] = 0
+                    temp_matrix[row_number][cell_number] = 0
 
     game_of_life_matrix = copy.deepcopy(temp_matrix)
     return game_of_life_matrix
-
-# init_world(world_size_x, world_size_y)
-# #get_game_world()
-# print(game_of_life_matrix)
-# #print(game_of_life_matrix_new_cell_state)
